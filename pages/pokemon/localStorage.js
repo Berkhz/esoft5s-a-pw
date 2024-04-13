e11059a489363afdfc0cefb40cd568373ae3af48
Kauan Bertalha <kauan.hb2004@gmail.com>
2024-04-12 23:22:36 -0300

    Up

--- pages/pokemon/localStorage.js
+++ pages/pokemon/localStorage.js
@@ -1,24 +1,32 @@
 
 async function QuantidadeVisitas() {
-    let contador = 0
-    let ultimaData = new Date().getDate()
+    let contador = 1
+    let ultimaData = new Date()
+    let recarregouPagina = document.lastModified
 
-    if (localStorage.length <= 0) {
+    if (localStorage !== null) {
         let visitaObject = { count: contador, data: ultimaData}
 
-        let contadorVisita = localStorage.setItem(contador, JSON.stringify(visitaObject))
-        let dataVisita = localStorage.setItem(ultimaData.toString(), JSON.stringify(visitaObject))
+        if(localStorage.getItem('contador')) {
+            contador = JSON.parse(localStorage.getItem('contador')).count + 1
+            visitaObject.count = contador
+        }
 
-        let lerContador = JSON.parse(localStorage.getItem(contadorVisita))
-        let lerDataVisita = JSON.parse(localStorage.getItem(dataVisita))
+        var dataTratada = new Intl.DateTimeFormat('pt-BR', {
+            dateStyle: 'short',
+            timeStyle: 'short',
+            timeZone: 'America/Belem',
+        });
+
+        localStorage.setItem('contador', JSON.stringify(visitaObject))
+        localStorage.setItem('ultimaData', JSON.stringify(visitaObject))
+   
+        let qtdVisitas = document.getElementById('qtdAcessos')
+        let contadorVisita = JSON.parse(localStorage.getItem('contador')).count
+        let dataVisita = JSON.parse(localStorage.getItem('ultimaData')).data
+        qtdVisitas.innerHTML = "Está página foi visitada: " + contadorVisita + 
+        " vezes. A Última visita foi: " + dataTratada.format(ultimaData)
     }
-    else {
-        let lerContador = JSON.parse(localStorage.getItem(contadorVisita))
-        let lerDataVisita = JSON.parse(localStorage.getItem(dataVisita))
-    }
-    
-    let qtdVisitas = document.getElementById('qtdAcessos')
-    qtdVisitas.innerHTML = "Está página foi visitada ${} vezes. A Última visita foi:"
 }
 
-QuantidadeVisitas()
+QuantidadeVisitas();
